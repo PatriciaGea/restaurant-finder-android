f25ce34bcd342175439a572613c47fb645e58644
PatriciaGea <patricia.gea@gmail.com>
2026-06-21 20:33:35 +0200

    version compileSdk = 37

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,11 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.restaurant_finder"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.example.restaurant_finder"
