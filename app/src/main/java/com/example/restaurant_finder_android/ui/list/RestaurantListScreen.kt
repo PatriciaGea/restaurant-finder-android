81ff580348478ec9ed92e10e53672d5c372dfc6d
PatriciaGea <patricia.gea@gmail.com>
2026-06-22 23:59:30 +0200

    fix: space lines cards

--- app/src/main/java/com/example/restaurant_finder_android/ui/list/RestaurantListScreen.kt
+++ app/src/main/java/com/example/restaurant_finder_android/ui/list/RestaurantListScreen.kt
@@ -249,7 +249,7 @@ fun RestaurantCard(
                     }
                 }
                 if (tagNames.isNotEmpty()) {
-                    Spacer(modifier = Modifier.height(2.dp))
+                    Spacer(modifier = Modifier.height(1.dp))
                     Text(
                         text = tagNames,
                         color = SubtitleGray,
@@ -259,7 +259,7 @@ fun RestaurantCard(
                         letterSpacing = 0.sp)
 
                 }
-                Spacer(modifier = Modifier.height(4.dp))
+                Spacer(modifier = Modifier.height(1.dp))
                 Row(verticalAlignment = Alignment.CenterVertically) {
                     Icon(
                         imageVector = Icons.Filled.Schedule,
