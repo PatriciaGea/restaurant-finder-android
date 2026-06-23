73413ac4605ca5d9bc563aa6cb2cc92f676c5d16
PatriciaGea <patricia.gea@gmail.com>
2026-06-23 17:40:48 +0200

    deleted imports not used

--- app/src/main/java/com/example/restaurant_finder_android/ui/detail/RestaurantDetailScreen.kt
+++ app/src/main/java/com/example/restaurant_finder_android/ui/detail/RestaurantDetailScreen.kt
@@ -3,17 +3,13 @@ package com.example.restaurant_finder_android.ui.detail
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
